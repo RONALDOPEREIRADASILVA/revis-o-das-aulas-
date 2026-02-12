97e26997cb80e575b6ab8e5db6db5290c2020e48
RONALDOPEREIRADASILVA <eletrotecnica.ronaldo@gmail.com>
2026-02-11 22:32:49 -0300

    Revisando e Praticando

--- praticando02/programa.js
+++ praticando02/programa.js
@@ -8,19 +8,33 @@ function Salvar() {
         console.log('digite o cadastro');
         alert('erro');
     } else {
-        (nome, telefone, email, 'cadastrado com sucesso')
-        document.querySelector(".Nome").innerHTML = +`Nome: ${nome} `;
+        (nome, telefone, email, 'cadastrado com sucesso');
+        document.querySelector(".item1").innerHTML = ` ${nome} `
+        document.querySelector(".item2").innerHTML = ` ${telefone}`
+        document.querySelector(".item3").innerHTML = `${email}`;
+
+
+        
+
+
+        // document.querySelector(".cadastros").innerHTML = `Nome: ${nome} , `;
+
+
+        /*
+        const cadastrado = [nome, telefone, email];
+        document.querySelector(".cadastros").innerHTML = ` cadastros: ${cadastrado}`;
+        console.log(cadastrado); */
+
+
 
     };
+
+
+
+    //document.querySelector(".Nome").innerHTML = `Nome: ${nome} `;
+
+
 };
-
-
-
-
-//document.querySelector(".Nome").innerHTML = +`Nome: ${nome} `;
-
-
-
 
 
 
