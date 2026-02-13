fab4ff33f08da1d6a533b2e6bc4258ba8b7d7f1a
RONALDOPEREIRADASILVA <eletrotecnica.ronaldo@gmail.com>
2026-02-13 14:12:27 -0300

    Praticando

--- aula01/script.js
+++ aula01/script.js
@@ -1,6 +1,15 @@
-var Ola = 'Ola mundo';
-var Ron ='Meu nome é Ronaldo';
+let ola = 'Ola mundo';
+//declarando a variavel "ola" e atribuindo o valor "ola mundo"
+let ron ='Meu nome é Ronaldo ';
+//declara a variavel "ron" e atribui o valor "Meu nome é Ronaldo"
 
 
+ 
+document.write( ola+'<br>' +ron);
 
-document.write( Ola+'<br>' +Ron);
+const nome = prompt("qual é seu nome")
+alert ("ola "  + nome);
+//cria um alerta no prompt e pede pra digitae o nome .
+//e retorna como resposta "ola mas o nome digitado"
+ 
+
